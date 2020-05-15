3e2837c98883bc3647e8005afaff7f9d03b9c9a4
deweechi <chipdeweese@gmail.com>
2020-05-14 21:59:49 -0400

    Adding Content API

---
+++ api/getdata.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config({ path: '.env' });
+
+const HOMEPAGE = process.env.HOMEPAGE;
+
+const app = express();
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  axios.get(HOMEPAGE)
+  .then(function (response) {
+    res.send(response.data)
+  })
+  .catch(function (error) {
+    res.status(404).send('Note Found')
+
+  });
+
+
+});
+module.exports = {
+  path:"/api/gethome",
+  handler:app
+}
+
