8d254a671368465ca1ff771f5f4f7196be1fbd9f
deweechi <chipdeweese@gmail.com>
2020-05-15 23:12:08 -0400

    Added markdown-it to render the markdown from strapi.

---
+++ components/markdown.vue
@@ -0,0 +1,21 @@
+<template lang="md">
+  <div v-html="renderedMd" />
+</template>
+
+<script>
+export default {
+  props: {
+    markdown: String
+  },
+  computed: {
+    renderedMd: function () {
+      let rawTemplate = '<div />'
+      if (this.markdown) {
+        rawTemplate = this.$md.render(this.markdown)
+      }
+      return rawTemplate
+
+    }
+  }
+}
+</script>
