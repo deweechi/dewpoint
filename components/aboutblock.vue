49bfff6116b4c612d6958001cc911fc9116d34ef
deweechi <chipdeweese@gmail.com>
2020-05-15 20:07:39 -0400

    Added About section and updated formating

---
+++ components/aboutblock.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="center">
+    <v-overlay absolute opacity=".65">
+      <div class="headline">{{ about.headline }}</div>
+      <div class="subtitle-1">{{ about.description }}</div>
+    </v-overlay>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    about: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
+
+<style></style>
