5d65427b83f00f36b78777c3ac5590aad4b6f958
deweechi <chipdeweese@gmail.com>
2020-05-19 14:20:53 -0400

    Added the begining of Authentication, Still needs fleshed out.

---
+++ pages/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <h1>Login</h1>
+    <UserAuthForm buttonText='Login' :submitForm='loginUser'> </UserAuthForm>
+  </v-container>
+</template>
+
+<script>
+import UserAuthForm from '~/components/UserAuthForm'
+
+
+export default {
+  components: {
+    UserAuthForm
+  },
+  methods: {
+    loginUser(){
+      alert('trying to login')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
